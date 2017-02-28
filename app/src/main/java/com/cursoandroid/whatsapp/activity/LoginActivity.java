9da0affaf6b92b67885678b040ded9bc87b8b827
RafaelNercessian <rafanercessian@gmail.com>
2017-02-27 22:06:25 -0300

    Permissao Android 6.0

--- app/src/main/java/com/cursoandroid/whatsapp/activity/LoginActivity.java
+++ app/src/main/java/com/cursoandroid/whatsapp/activity/LoginActivity.java
@@ -1,14 +1,18 @@
 package com.cursoandroid.whatsapp.activity;
 
+import android.Manifest;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.telephony.SmsMessage;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import com.cursoandroid.whatsapp.R;
+import com.cursoandroid.whatsapp.helper.Permissao;
 import com.cursoandroid.whatsapp.helper.Preferencias;
 import com.github.rtoshiro.util.format.SimpleMaskFormatter;
 import com.github.rtoshiro.util.format.text.MaskTextWatcher;
@@ -23,45 +27,65 @@ public class LoginActivity extends Activity {
     private EditText ddi;
     private Button botao;
     private EditText nome;
+    private String[] permissoes=new String[]{
+            Manifest.permission.SEND_SMS,
+            Manifest.permission.RECEIVE_SMS
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        telefone=(EditText) findViewById(R.id.login_telefone);
-        ddd= (EditText) findViewById(R.id.login_ddd);
-        ddi= (EditText) findViewById(R.id.login_ddi);
-        botao=(Button) findViewById(R.id.login_botao);
-        nome=(EditText) findViewById(R.id.login_nome);
+        telefone = (EditText) findViewById(R.id.login_telefone);
+        ddd = (EditText) findViewById(R.id.login_ddd);
+        ddi = (EditText) findViewById(R.id.login_ddi);
+        botao = (Button) findViewById(R.id.login_botao);
+        nome = (EditText) findViewById(R.id.login_nome);
 
         inserindoMascaraNosEditText();
 
         botao.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String nomeUsuario=nome.getText().toString();
-                String telefoneCompleto=ddi.getText().toString()+ddd.getText().toString()+telefone.getText().toString();
-                String telefoneSemMais=telefoneCompleto.replace("+","");
+                String nomeUsuario = nome.getText().toString();
+                String telefoneCompleto = ddi.getText().toString() + ddd.getText().toString() + telefone.getText().toString();
+                String telefoneSemMais = telefoneCompleto.replace("+", "");
                 String telefoneSemFormatcao = telefoneSemMais.replace("-", "");
-                
-                
+
+                Permissao.validaPermissoes(LoginActivity.this,permissoes);
+
+
                 //Gerando token
                 Random random = new Random();
-                int numeroRandomico = random.nextInt(9999-1000)+1000;
-                String token=String.valueOf(numeroRandomico);
+                int numeroRandomico = random.nextInt(9999 - 1000) + 1000;
+                String token = String.valueOf(numeroRandomico);
+                String mensagemEnvio="Whatsapp código de confirmação "+token;
 
                 //Salvar os dados para validação
                 Preferencias preferencias = new Preferencias(LoginActivity.this);
-                preferencias.salvarUsuarioPreferencias(nomeUsuario,telefoneSemFormatcao,token);
+                preferencias.salvarUsuarioPreferencias(nomeUsuario, telefoneSemFormatcao, token);
 
                 HashMap<String, String> usuario = preferencias.getDadosUsuario();
-                Log.i("Nome",usuario.get("nome"));
-                Log.i("telefone",usuario.get("telefone"));
-                Log.i("token",usuario.get("token"));
+
+                //Envio do SMS
+                telefoneSemFormatcao="5554";
+                boolean enviadoSMS = enviaSMS("+" + telefoneSemFormatcao, mensagemEnvio);
 
             }
         });
+    }
+
+    private boolean enviaSMS(String telefone, String mensagem) {
+
+        try {
+            SmsManager smsManager = SmsManager.getDefault();
+            smsManager.sendTextMessage(telefone, null, mensagem, null, null);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 
     private void inserindoMascaraNosEditText() {
