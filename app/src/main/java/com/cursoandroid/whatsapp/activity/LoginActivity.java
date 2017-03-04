5f68998509f40846eb9aea5ac9b2c5059b61d91d
RafaelNercessian <rafanercessian@gmail.com>
2017-03-04 13:13:57 -0300

    Adicionando toolbar

--- app/src/main/java/com/cursoandroid/whatsapp/activity/LoginActivity.java
+++ app/src/main/java/com/cursoandroid/whatsapp/activity/LoginActivity.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -20,8 +21,13 @@ import android.widget.Toast;
 import com.cursoandroid.whatsapp.R;
 import com.cursoandroid.whatsapp.helper.Permissao;
 import com.cursoandroid.whatsapp.helper.Preferencias;
+import com.cursoandroid.whatsapp.modelo.Usuario;
 import com.github.rtoshiro.util.format.SimpleMaskFormatter;
 import com.github.rtoshiro.util.format.text.MaskTextWatcher;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.HashMap;
 import java.util.Random;
@@ -29,11 +35,25 @@ import java.util.Random;
 public class LoginActivity extends Activity {
 
     private TextView abrirCadastroUsuario;
+    private FirebaseAuth mAuth;
+    private Button botao;
+    private EditText email;
+    private EditText senha;
+    private Usuario usuario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+        if(mAuth.getCurrentUser()!=null){
+            chamaMainActivity();
+        }
+
+        botao=(Button) findViewById(R.id.login_botao);
+        email=(EditText) findViewById(R.id.login_email);
+        senha=(EditText) findViewById(R.id.login_senha);
 
         abrirCadastroUsuario=(TextView) findViewById(R.id.login_cadastro);
         abrirCadastroUsuario.setOnClickListener(new View.OnClickListener() {
@@ -44,6 +64,39 @@ public class LoginActivity extends Activity {
             }
         });
 
+        botao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                usuario = new Usuario();
+                usuario.setEmail(email.getText().toString());
+                usuario.setSenha(senha.getText().toString());
+                autenticaUsuario();
+            }
+        });
+
+    }
+
+    private void autenticaUsuario() {
+        mAuth.signInWithEmailAndPassword(usuario.getEmail(),usuario.getSenha())
+                .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            chamaMainActivity();
+                        }else{
+                            Toast.makeText(LoginActivity.this,"Não existe usuário",Toast.LENGTH_SHORT).show();
+
+                        }
+
+                    }
+                });
+
+    }
+
+    private void chamaMainActivity() {
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 
 }
--- app/src/main/java/com/cursoandroid/whatsapp/activity/MainActivity.java
+++ app/src/main/java/com/cursoandroid/whatsapp/activity/MainActivity.java
@@ -1,20 +1,22 @@
 package com.cursoandroid.whatsapp.activity;
 
-import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 import com.cursoandroid.whatsapp.R;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
-public class MainActivity extends Activity {
-
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
